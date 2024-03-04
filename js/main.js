d744886f888caf3a50557e812a9cdef7c056b03f
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-04 21:57:29 +0530

    chapter2_link to HTML

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+console.log(typeof "dude");
+console.log(typeof 88);
+console.log(typeof true);
+console.log("ok byeee!");
+let myNumber = 77;
+let result = myNumber + 100;
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
